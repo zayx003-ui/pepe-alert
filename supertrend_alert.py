21d3f9a039d68a8d9c2c313aa8c90156736574f8
zayx003-ui <zayx003@gmail.com>
2026-08-02 20:56:37 +0200

    Update supertrend_alert.py

--- supertrend_alert.py
+++ supertrend_alert.py
@@ -19,8 +19,8 @@ ATR_PERIOD = 10
 MULTIPLIER = 3
 STATE_FILE = "state.json"  # garde en mémoire le dernier signal envoyé
 
-TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
-TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+TELEGRAM_TOKEN = os.environ["8815996302:AAG2539ZhxQhhdKSLgQI7nb4ltkgGUzEsYU"]
+TELEGRAM_CHAT_ID = os.environ["7243865548"]
 
 
 def get_klines(symbol, interval, limit=100):
