703257d52199ba4b39fda9a37c55ff3b4d31b686
zayx003-ui <zayx003@gmail.com>
2026-08-04 08:36:31 +0200

    Update supertrend_alert.py

--- supertrend_alert.py
+++ supertrend_alert.py
@@ -16,7 +16,7 @@ from cryptography.hazmat.backends import default_backend
 
 # ---------- CONFIG SIGNAL ----------
 SYMBOL = "PEPE-USDT"      # Source du signal (KuCoin, historique fiable)
-INTERVAL = "1min"
+INTERVAL = "5min"
 ATR_PERIOD = 10
 MULTIPLIER = 3
 STATE_FILE = "state.json"
