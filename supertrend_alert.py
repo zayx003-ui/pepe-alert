f1ae32d2db346d54913fb0ac7b5da4c2fb27d4f4
zayx003-ui <zayx003@gmail.com>
2026-08-04 13:48:38 +0200

    Update supertrend_alert.py

--- supertrend_alert.py
+++ supertrend_alert.py
@@ -16,7 +16,7 @@ from cryptography.hazmat.backends import default_backend
 
 # ---------- CONFIG SIGNAL ----------
 SYMBOL = "PEPE-USDT"
-INTERVAL = "5min"
+INTERVAL = "30min"
 ATR_PERIOD = 10
 MULTIPLIER = 3
 STATE_FILE = "state.json"
